822bba8fb3ddee733e7008b4c6f085b028ce6596
78y9ysadf <blair.taylor@athletics.org.au>
2024-11-26 17:10:31 +1100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'my-squarespace-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+// Install Event: Cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Event: Serve cached resources
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
